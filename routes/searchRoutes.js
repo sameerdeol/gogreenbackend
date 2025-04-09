15a40c3404134b93c52e900803a3e63427fde7f0
ashu6879 <ashup8998@gmail.com>
2025-04-09 16:22:42 +0530

    add discount module done

--- routes/searchRoutes.js
+++ routes/searchRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const { verifyToken } = require('../middleware/authroization'); // ✅ fixed typo
 const { searchProduct } = require('../controllers/searchProductController');
 
-router.post('/searchproducts', searchProduct); // apply if needed
+router.post('/searchproducts', verifyToken ,searchProduct); // apply if needed
 
 module.exports = router;
