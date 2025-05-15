53e12a77fb6118f64be19e8add0fd912842101c6
ashu6879 <ashup8998@gmail.com>
2025-05-15 18:51:45 +0530

    temp order accept

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -4,7 +4,7 @@ const { verifyToken } = require('../middleware/authroization');
 const {createOrder, getOrdersByUserId, acceptOrder} = require("../controllers/orderController");
  
 router.post("/createorder",  verifyToken, createOrder);
-router.put('/acceptorders',  acceptOrder);
+router.put('/acceptorders', verifyToken, acceptOrder);
 router.post('/getorderbyuserID', verifyToken, getOrdersByUserId);
  
 module.exports = router;
