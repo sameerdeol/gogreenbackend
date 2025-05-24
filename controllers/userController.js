2502ac450b165405f7acf6ace0e76f7e0b9e4479
ashu6879 <ashup8998@gmail.com>
2025-05-24 10:36:13 +0530

    issue solved

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const {User} = require('../models/User');
 const {generateUniqueUsername} = require('../middleware/username');
 const path = require('path');
 const uploadFields = require('../middleware/multerConfig'); // Import Multer setup
