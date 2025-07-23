b31f964f3005492b5f1b212780a5b32c1d3e5ec2
ashu6879 <ashup8998@gmail.com>
2025-07-23 15:48:55 +0530

    update\

--- models/User.js
+++ models/User.js
@@ -598,7 +598,7 @@ const User = {
             FROM users u
             JOIN vendors v ON v.user_id = u.id
             LEFT JOIN favourite_vendors fv ON fv.vendor_id = v.user_id AND fv.user_id = ?
-            WHERE u.is_verified = 1
+            WHERE u.is_verified = 1 and u.status = 1
             AND EXISTS (
                 SELECT 1 FROM products p WHERE p.vendor_id = v.user_id
             );
