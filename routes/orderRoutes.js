61d020122cc22f7ae2fabf5b93eb6bbd487dd3c7
ashu6879 <ashup8998@gmail.com>
2025-07-24 13:33:59 +0530

    added verifiacation token

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -5,15 +5,15 @@ const {} = require('../middleware/checkManagerRoll');
 const {checkManagerRole} = require('../middleware/checkManagerRoll');
 const {createOrder, getOrdersByUserId,  updateOrderStatus, getOrdersByVendorId, getOrderDetails, updateOrderTiming, verifyOtp, getAllOrders, orderHistory} = require("../controllers/orderController");
  
-router.post("/createorder",   createOrder);
-router.put('/updateorderstatus',  updateOrderStatus);
+router.post("/createorder", verifyToken,  createOrder);
+router.put('/updateorderstatus', verifyToken,  updateOrderStatus);
 router.post('/getorderbyuserID', verifyToken, getOrdersByUserId);
-router.post('/getorderdetails', getOrderDetails);
+router.post('/getorderdetails', verifyToken, getOrderDetails);
 router.post('/getallorderbyvendorid', verifyToken, getOrdersByVendorId);
-router.post('/list', getAllOrders);
-router.post('/updateordertiming', updateOrderTiming);
+router.post('/list', verifyToken, getAllOrders);
+router.post('/updateordertiming', verifyToken, updateOrderTiming);
 router.post('/verifyotprider', verifyOtp);
-router.post('/orderhistorybyuserid', orderHistory);
+router.post('/orderhistorybyuserid', verifyToken, orderHistory);
 
  
 module.exports = router;
