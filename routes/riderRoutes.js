51d8321d02fba56bb30bd5cbb0655bb7c6e5bdc9
ashu6879 <ashup8998@gmail.com>
2025-07-24 13:14:14 +0530

    added verifiacation token

--- routes/riderRoutes.js
+++ routes/riderRoutes.js
@@ -4,20 +4,20 @@ const uploadFields = require('../middleware/multerConfig');
 const { verifyToken } = require('../middleware/authroization');
 const router = express.Router();
 
-router.post('/rider-verification', uploadFields, riderController.riderVerification);
-router.post('/rider-personaldetails', uploadFields, riderController.riderPersonalDetails);
+router.post('/rider-verification', verifyToken, uploadFields, riderController.riderVerification);
+router.post('/rider-personaldetails', verifyToken, uploadFields, riderController.riderPersonalDetails);
 router.post('/update-riderPassword', verifyToken, riderController.updateRiderProfile);
 router.put('/update-riderProfile', verifyToken, uploadFields, riderController.updateRiderProfile);
 router.post('/rider-login', riderController.riderLogin);
 router.post('/rider-signup', riderController.riderSignup);
-router.post('/rider-profile', riderController.riderProfile);
-router.post('/rider-vehicledetails', riderController.vehicleDetails);
+router.post('/rider-profile', verifyToken, riderController.riderProfile);
+router.post('/rider-vehicledetails', verifyToken, riderController.vehicleDetails);
 router.post('/rider-status', verifyToken, riderController.riderStatus);
 router.post('/send-riderOtp', (req, res) => {}); // If needed, implement in riderController
 router.post('/reset-riderPwd', (req, res) => {}); // If needed, implement in riderController
 router.put('/chnage-riderPwd', (req, res) => {}); // If needed, implement in riderController
 router.put('/updateRider-location', verifyToken, riderController.updateRiderLocation);
-router.get('/getallridersforadmin', riderController.allRidersforAdmin);
-router.get('/getallridersforadminbyID/:rider_id', riderController.allRidersforAdminbyRiderID);
+router.get('/getallridersforadmin', verifyToken, riderController.allRidersforAdmin);
+router.get('/getallridersforadminbyID/:rider_id', verifyToken, riderController.allRidersforAdminbyRiderID);
 
 module.exports = router; 
--- routes/vendorRoutes.js
+++ routes/vendorRoutes.js
@@ -5,25 +5,26 @@ const { verifyToken } = require('../middleware/authroization');
 const router = express.Router();
 
 // Vendor Type CRUD Endpoints
-router.post('/type', uploadFields, vendorController.createVendorType);
-router.get('/type', vendorController.getAllVendorTypes);
-router.put('/type/:id', uploadFields, vendorController.updateVendorType);
-router.delete('/type/:id', vendorController.deleteVendorType);
+router.post('/type', verifyToken, uploadFields, vendorController.createVendorType);
+router.get('/type', verifyToken, vendorController.getAllVendorTypes);
+router.put('/type/:id', verifyToken, uploadFields, vendorController.updateVendorType);
+router.delete('/type/:id', verifyToken, vendorController.deleteVendorType);
 
-router.post('/vendor-verification', uploadFields, vendorController.vendorVerification);
+// Vendor Operations
+router.post('/vendor-verification', verifyToken, uploadFields, vendorController.vendorVerification);
 router.post('/update-vendorPassword', verifyToken, vendorController.updateVendorProfile);
 router.put('/update-vendorProfile', verifyToken, uploadFields, vendorController.updateVendorProfile);
-router.post('/vendor-login', vendorController.vendorLogin);
-router.post('/vendor-signup', vendorController.vendorSignup);
+router.post('/vendor-login',  vendorController.vendorLogin); // Optional - remove verifyToken if login is public
+router.post('/vendor-signup',  vendorController.vendorSignup); // Optional - remove verifyToken if signup is public
 router.post('/vendor-profile', verifyToken, vendorController.vendorProfile);
 router.post('/vendor-status', verifyToken, vendorController.vendorStatus);
-router.post('/send-vendorOtp', (req, res) => {}); // If needed, implement in vendorController
-router.post('/reset-vendorPwd', (req, res) => {}); // If needed, implement in vendorController
-router.put('/chnage-vendorPwd', (req, res) => {}); // If needed, implement in vendorController
+router.post('/send-vendorOtp',  (req, res) => {}); // If needed, implement in vendorController
+router.post('/reset-vendorPwd',  (req, res) => {}); // If needed, implement in vendorController
+router.put('/chnage-vendorPwd', verifyToken, (req, res) => {}); // If needed, implement in vendorController
 router.post('/all-vendors', verifyToken, vendorController.allVendors);
-router.post('/store-bussinessdetails', uploadFields, vendorController.storeBusinessDetails);
-router.post('/store-additionaldetails', uploadFields, vendorController.storeAdditionalDetails);
-router.get('/getallvendorsforadmin', vendorController.allVendorsforAdmin);
-router.get('/getallvendorsforadminbyID/:vendor_id', vendorController.allVendorsforAdminbyVendorID);
+router.post('/store-bussinessdetails', verifyToken, uploadFields, vendorController.storeBusinessDetails);
+router.post('/store-additionaldetails', verifyToken, uploadFields, vendorController.storeAdditionalDetails);
+router.get('/getallvendorsforadmin', verifyToken, vendorController.allVendorsforAdmin);
+router.get('/getallvendorsforadminbyID/:vendor_id', verifyToken, vendorController.allVendorsforAdminbyVendorID);
 
-module.exports = router; 
+module.exports = router;
