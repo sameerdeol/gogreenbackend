4e04261ba2eecbe48fc2fdc4e5f0f957809311b0
ashu6879 <ashup8998@gmail.com>
2025-07-28 11:22:53 +0530

    test

--- routes/productDiscount.js
+++ routes/productDiscount.js
@@ -4,13 +4,12 @@ const router = express.Router();
 const { checkManagerRole } = require('../middleware/checkManagerRoll');
 const { verifyToken } = require('../middleware/authroization');
 const {
-    createDiscount,
-    updateDiscount,
+    createOrUpdateDiscount,
     deleteDiscount,
     getDiscounts
 } = require('../controllers/discountController');
 
-router.post('/add-update-discount', checkManagerRole, createDiscount);
+router.post('/add-update-discount', checkManagerRole, createOrUpdateDiscount);
 router.get('/get-discounts', checkManagerRole, getDiscounts);
 router.delete('/delete-discount', checkManagerRole, deleteDiscount);
 
