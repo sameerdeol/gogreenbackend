5ea64e8d109c446580160d2fb957a13c9293206f
ashu6879 <ashup8998@gmail.com>
2025-07-30 12:55:18 +0530

    add token

--- models/fcmTokenModel.js
+++ models/fcmTokenModel.js
@@ -2,8 +2,7 @@ const db = require('../config/db');
 
 const UserFcmToken = {
     create: (user_id, fcmToken, callback) => {
-        const sql = `INSERT IGNORE INTO users_fcm_token (user_id, fcm_token)
-                     VALUES (?, ?)`;
+        const sql = `INSERT IGNORE INTO users_fcm_token (user_id, fcm_token) VALUES (?, ?)`;;
         db.query(sql, [user_id, fcmToken], callback);
     },
     getTokenByUserId: (user_id, callback) => {
