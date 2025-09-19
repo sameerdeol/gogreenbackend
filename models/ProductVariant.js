074528cedde753908acb7606d1dd76f658dcb98e
ashu6879 <ashup8998@gmail.com>
2025-09-19 12:11:25 +0530

    add product varinat available check

--- models/ProductVariant.js
+++ models/ProductVariant.js
@@ -2,10 +2,11 @@ const db = require('../config/db');
 
 const ProductVariant = {
     create: (productId, variants, callback) => {
-        const values = variants.map(v => [productId, v.type, v.value, parseFloat(v.price)]);
-        const sql = 'INSERT INTO product_variants (product_id, type, value, price) VALUES ?';
+        const values = variants.map(v => [productId, v.type, v.value, parseFloat(v.price), v.is_available]);
+        const sql = 'INSERT INTO product_variants (product_id, type, value, price, is_available) VALUES ?';
         db.query(sql, [values], callback);
     },
+
     deleteByProductId: (productId, callback) => {
         const sql = 'DELETE FROM product_variants WHERE product_id = ?';
         db.query(sql, [productId], callback);
