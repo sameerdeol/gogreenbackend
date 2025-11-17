2231b1eede6f6c7af0137450455622cad65c4b6b
sameer <deolsameer707@gmail.com>
2025-11-17 15:36:24 +0530

    adding chatcontroller 17-11-25

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -4,12 +4,7 @@ const router = express.Router();
 const { verifyToken } = require('../middleware/authroization');
 const { getChat, saveChatMessage } = require("../controllers/chatController");
 
-// Attach io to req inside server.js instead
 router.post("/messages", verifyToken, getChat);
-
-router.post("/send", verifyToken, (req, res) => {
-  const io = req.app.get("socketio");   // <<< we access io this way
-  saveChatMessage(req, res, io);
-});
+router.post("/send", verifyToken, saveChatMessage);
 
 module.exports = router;
