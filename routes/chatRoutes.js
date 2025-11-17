396afc46092ff9ba62c00589b9032c53ae2d24f1
sameer <deolsameer707@gmail.com>
2025-11-17 15:05:23 +0530

    adding chatcontroller 17-11-25

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -1,27 +1,10 @@
-// const express = require("express");
-// const router = express.Router();
 
-// const { verifyToken } = require('../middleware/authroization');
-// const { getChat, saveChatMessage } = require("../controllers/chatController");
-
-// module.exports = (io) => {
-
-//   // ⭐ FIX: GET → POST
-//   router.post("/messages", verifyToken, getChat);
-
-//   router.post("/send", verifyToken, (req, res) =>
-//     saveChatMessage(req, res, io)
-//   );
-
-//   return router;
-// };
 const { verifyToken } = require('../middleware/authroization');
 const express = require('express');
 const router = express.Router();
 const {checkManagerRole} = require('../middleware/checkManagerRoll');
 const { getChat, saveChatMessage } = require("../controllers/chatController");
 
-// Routes for User Addresses
   // ⭐ FIX: GET → POST
   router.post("/messages", verifyToken, getChat);
 
