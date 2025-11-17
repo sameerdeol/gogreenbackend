f76074cb0cc0e42a842eb988cb4ce6aa6a2c9bc9
sameer <deolsameer707@gmail.com>
2025-11-17 11:55:47 +0530

    adding chatcontroller 17-11-25

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -4,13 +4,11 @@ const router = express.Router();
 const { verifyToken } = require('../middleware/authroization');
 const { getChat, saveChatMessage } = require("../controllers/chatController");
 
-// Export router with access to io
 module.exports = (io) => {
 
-  // ⭐ Get chat history for a specific ride + rider
-  router.get("/messages", verifyToken, getChat);
+  // ⭐ FIX: GET → POST
+  router.post("/messages", verifyToken, getChat);
 
-  // ⭐ Save message (if you also want REST API version besides socket)
   router.post("/send", verifyToken, (req, res) =>
     saveChatMessage(req, res, io)
   );
